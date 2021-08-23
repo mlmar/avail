28b2f619c159d914e9d107dd51bdc7a4e0bfe21d
mlmar <marcloussm@gmail.com>
2021-08-23 16:21:46 -0700

    date pickers

--- event-react/src/js/modules/Create.js
+++ event-react/src/js/modules/Create.js
@@ -1,6 +1,23 @@
-import { Panel } from "./ui/Interface";
+import { useState } from 'react';
+import { Panel } from './ui/Interface';
 
 const Create = () => {
+  const [dates, setDates] = useState([new Date()]);
+
+  const handleDateChange = (event) => {
+    const { id, value } = event.target;
+
+    setDates((prev) => {
+      let changes = [...prev];
+      changes[id] = new Date(value);
+      return changes;
+    });
+  }
+
+  const handleAddDate = () => {
+    setDates((prev) => [...prev, new Date(dates[dates.length-1].valueOf())]);
+  }
+
   return (
     <Panel className="create flex-col">
       {/* <label className="subtitle medium bold"> Create an Event </label> */}
@@ -12,8 +29,8 @@ const Create = () => {
 
       <div className="flex-col">
         <label className="medium"> Event Date </label>
-        <input type="date"/>
-        <button> + Add Another Date </button>
+        { dates.map((date, i) => <input type="date" value={date.toLocaleDateString('en-CA')} id={i} onChange={handleDateChange} key={i}/> )}
+        <button onClick={handleAddDate}> + Add Another Date </button>
       </div>
 
       <button> Create Event </button>
