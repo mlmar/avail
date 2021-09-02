a16e2ba2c49a8240b8a46c3a4b3f21e58b9a2add
mlmar <marcloussm@gmail.com>
2021-09-02 01:53:36 -0700

    test deployment

--- event-react/src/js/util/System.js
+++ event-react/src/js/util/System.js
@@ -4,9 +4,10 @@ const LOCAL_CLIENT = "http://localhost:3000";
 // const LOCAL_SERVER = "http://localhost:3300";
 const LOCAL_SERVER = "http://192.168.1.74:3300";
 
-const DEPLOYED = null;
+const DEPLOYED_HOME = "https://avail.vercel.app";
+const DEPLOYED_SERVER = "https://avail-auth.vercel.app";
 
-export const HOME_URL = DEV ? LOCAL_CLIENT : DEPLOYED;
-export const SERVER_URL = DEV ? LOCAL_SERVER : DEPLOYED;
+export const HOME_URL = DEV ? LOCAL_CLIENT : DEPLOYED_HOME;
+export const SERVER_URL = DEV ? LOCAL_SERVER : DEPLOYED_SERVER;
 
-export const STRIPPED_HOME_URL = HOME_URL.replace("http://", "").replace("https://", "");
+export const STRIPPED_HOME_URL = HOME_URL?.replace("http://", "").replace("https://", "");
